095c5f16dac4cd2edd6a3927c72edd8040e388c0
rabiussani <sanikhan.cse@gmail.com>
2024-12-20 04:55:13 +0600

    template one three added

--- src/Components/Tabs/Tabs.tsx
+++ src/Components/Tabs/Tabs.tsx
@@ -1,8 +1,5 @@
 import {
-  Badge,
   Button,
-  EmbedCard,
-  Pill,
   Rows,
   Tab,
   TabList,
@@ -10,19 +7,14 @@ import {
   TabPanels,
   Tabs,
   Text,
-  Title,
 } from "@canva/app-ui-kit";
 import { addPage } from "@canva/design";
 import { useIntl } from "react-intl";
 import { getCroppedImageDataUrl } from "utils/cropImage";
-import { useAddElement } from "utils/use_add_element";
+import { templateOneGenerate } from "../../helper/templateOne";
+import { templateThreeGenerate } from "../../helper/templateThree";
 
 export default function AppTabs() {
-  const addElement = useAddElement();
-
-  const handlePillClick = () => {
-    console.log("Click");
-  };
 
   const onClick = async () => {
     const url = getCroppedImageDataUrl(
@@ -59,7 +51,7 @@ export default function AppTabs() {
               },
               top: 0,
               left: 10,
-              height: 160,
+              height: 140,
               width: 300,
               dataUrl:
                 "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABEAAAARCAYAAAA7bUf6AAAAAXNSR0IArs4c6QAAAdRJREFUOE9dlAFrE0EUhGfzM/OLCoJYKoFAqbR0L6UoiKLoT9Pse1Nn3t41NXDcBfa+nZk3e+3z0++eADICJPWAiAAYQAzkCMQYiPgLnv943S4DAL2mAWiGkPvMBHUxCqiFoWeBzohx9p1xBpho6wUs7fn0q5Pck4kCBSiIlQU4JiQKkmMUABMkyNPpZ0cKQgMEkgLD9D+GQbFBzmgkdhOCgvwoyLSzqdkUDau6hOzIUqING5Z2evzeCeyxQQL1PNXY2qpEwOHwmzKzLSytP3zrAPewnQr2FZJ+yUoc6rA9TU0gQQAK8rXDwWq8guRbSFYmnpKB6z3mhLi0x09fOqXEgFLjHf6zlAp2qtKEbMt2uLSH2+eCcPYkVaK3oNSUph1lpfJJnUZNZXJ/7D2h6ejlNZcJmhkpk1JSIVePViVY2t3hvpOoTES+zGZ2x32REvdmAhws0Yil3d7czcZOJbYygclqrcatcBXyeiRSjVVfuLTj+6N74qloZwOmHUx7Lt7YlCj0Oj+EG3t4d+hE7uFd62o+oVpQMAW7KvDBNIRQc93Yj1fXr41dXxR/wtbjsDXYNi4gsnNz9aHDxUvfvDuAnXbTd8ZBj2qypjQrsAPr7Pz7vQBKj7pE+J6clQAAAABJRU5ErkJggg==",
@@ -70,7 +62,7 @@ export default function AppTabs() {
                 text: "Hello",
                 decorative: false,
               },
-              top: 137,
+              top: 120,
               left: 255,
               height: 40,
               width: 40,
@@ -80,7 +72,7 @@ export default function AppTabs() {
             {
               type: "text",
               children: ["ALEX PAPA"],
-              top: 190,
+              top: 175,
               left: 240,
               fontSize: 8,
               fontWeight: "normal",
@@ -90,7 +82,7 @@ export default function AppTabs() {
             {
               type: "text",
               children: ["17253947645"],
-              top: 200,
+              top: 185,
               left: 240,
               fontSize: 8,
               fontWeight: "normal",
@@ -103,17 +95,17 @@ export default function AppTabs() {
         {
           type: "text",
           children: ["VAUCLUSE"],
-          top: 200,
+          top: 180,
           left: 10,
           fontSize: 7,
-          fontWeight: "normal",
+          fontWeight: "bold",
           width: 100,
           textAlign: "start",
         },
         {
           type: "text",
           children: ["3 MAYALL AVENUE"],
-          top: 210,
+          top: 190,
           left: 10,
           fontSize: 7,
           width: 100,
@@ -121,7 +113,7 @@ export default function AppTabs() {
         },
         {
           type: "group",
-          top: 224,
+          top: 204,
           left: 10,
           width: 50,
           height: "auto",
@@ -184,44 +176,45 @@ export default function AppTabs() {
         },
         {
           type: "group",
-          top: 226,
-          left: 10,
-          width: 70,
-          height: "auto",
+          top: 220,
+          left: 0,
+          width: 120,
+          height: 25,
           children: [
             {
-              type: "shape",
-              top: 220,
-              left: 10,
-              width: 60,
+              top: 0,
+              left: 0,
+              width: 120,
               height: 25,
-              viewBox: {
-                top: 0,
-                left: 10,
-                width: 60,
-                height: 23,
-              },
+              type: "shape",
               paths: [
                 {
-                  d: "M10 10 h80 a10 10 0 0 1 10 10 v30 a10 10 0 0 1 -10 10 h-80 a10 10 0 0 1 -10 -10 v-30 a10 10 0 0 1 10 -10 Z",
+                  d: "M 12.5 0 H 107.5 A 12.5 12.5 0 0 1 120 12.5 V 12.5 A 12.5 12.5 0 0 1 107.5 25 H 12.5 A 12.5 12.5 0 0 1 0 12.5 V 12.5 A 12.5 12.5 0 0 1 12.5 0 Z",
                   fill: {
-                    color: "#ff0099",
+                    color: "#F87F0F",
                   },
                 },
               ],
+              viewBox: {
+                top: 0,
+                left: 0,
+                width: 120,
+                height: 25,
+              },
             },
             {
               type: "text",
-              children: ["Enquery Now"],
-              top: 235,
-              left: 20,
-              fontSize: 6,
-              width: 70,
-              textAlign: "start",
-              color: "#f1f1f1",
+              children: ["Enquire Now"],
+              top: 6,
+              left: 0,
+              fontSize: 12,
+              width: 120,
+              textAlign: "center",
+              color: "#ffffff",
             },
           ],
         },
+
       ],
     });
   };
@@ -402,48 +395,27 @@ export default function AppTabs() {
           <TabPanels>
             <TabPanel id="template">
               <Rows spacing="2u">
-                <Rows spacing="1u">
-                  <Pill
-                    ariaLabel="a pill"
-                    onClick={handlePillClick}
-                    // start={() => {}}
-                    text="Configure Your Branding"
-                    // selected
-                    showTooltip={{
-                      disabled: true,
-                    }}
-                    role="button"
-                  />
-                </Rows>
-
-                <Rows spacing="0.5u">
-                  <Title size="xsmall">Property Template</Title>
-                  <Text size="small">First Template Showing Here.</Text>
-                  <EmbedCard
-                    ariaLabel="Add embed to design"
-                    bottomEnd={<Badge text="custom" tone="contrast" />}
-                    description="Puppyhood"
-                    onClick={() => {}}
-                    onDragStart={() => {}}
-                    thumbnailUrl="https://www.canva.dev/example-assets/images/puppyhood.jpg"
-                    title="Heartwarming Chatter: Adorable Conversation with a puppy"
-                  />
-                </Rows>
-
-                <Button variant="primary" onClick={onClickTemplate4} stretch>
-                  {intl.formatMessage({
-                    defaultMessage: "Template 4",
-                    description:
-                      "Button text to do something cool. Creates a new text element when pressed.",
-                  })}
+                <Button variant="primary" onClick={templateOneGenerate} stretch>
+                  Template 1 (300 * 600)
                 </Button>
                 <Button variant="primary" onClick={onClick} stretch>
                   {intl.formatMessage({
-                    defaultMessage: "Template 2",
+                    defaultMessage: "Template 2 (300 * 250)",
                     description:
                       "Button text to do something cool. Creates a new text element when pressed.",
                   })}
                 </Button>
+                <Button variant="primary" onClick={templateThreeGenerate} stretch>
+                  Template 3 (970 * 250)
+                </Button>
+                <Button variant="primary" onClick={onClickTemplate4} stretch>
+                  {intl.formatMessage({
+                    defaultMessage: "Template 4 (600 * 600)",
+                    description:
+                      "Button text to do something cool. Creates a new text element when pressed.",
+                  })}
+                </Button>
+
               </Rows>
             </TabPanel>
             <TabPanel id="videos">
---
+++ src/helper/templateThree.ts
@@ -0,0 +1,214 @@
+import { addPage } from "@canva/design";
+export const templateThreeGenerate = async () => {
+  await addPage({
+    elements: [
+      {
+        type: "image",
+        altText: {
+          text: "Hello",
+          decorative: false,
+        },
+        top: 0,
+        left: 0,
+        height: 250,
+        width: 380,
+        dataUrl:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABEAAAARCAYAAAA7bUf6AAAAAXNSR0IArs4c6QAAAdRJREFUOE9dlAFrE0EUhGfzM/OLCoJYKoFAqbR0L6UoiKLoT9Pse1Nn3t41NXDcBfa+nZk3e+3z0++eADICJPWAiAAYQAzkCMQYiPgLnv943S4DAL2mAWiGkPvMBHUxCqiFoWeBzohx9p1xBpho6wUs7fn0q5Pck4kCBSiIlQU4JiQKkmMUABMkyNPpZ0cKQgMEkgLD9D+GQbFBzmgkdhOCgvwoyLSzqdkUDau6hOzIUqING5Z2evzeCeyxQQL1PNXY2qpEwOHwmzKzLSytP3zrAPewnQr2FZJ+yUoc6rA9TU0gQQAK8rXDwWq8guRbSFYmnpKB6z3mhLi0x09fOqXEgFLjHf6zlAp2qtKEbMt2uLSH2+eCcPYkVaK3oNSUph1lpfJJnUZNZXJ/7D2h6ejlNZcJmhkpk1JSIVePViVY2t3hvpOoTES+zGZ2x32REvdmAhws0Yil3d7czcZOJbYygclqrcatcBXyeiRSjVVfuLTj+6N74qloZwOmHUx7Lt7YlCj0Oj+EG3t4d+hE7uFd62o+oVpQMAW7KvDBNIRQc93Yj1fXr41dXxR/wtbjsDXYNi4gsnNz9aHDxUvfvDuAnXbTd8ZBj2qypjQrsAPr7Pz7vQBKj7pE+J6clQAAAABJRU5ErkJggg==",
+      },
+      {
+        type: "text",
+        children: ["VAUCLUSE"],
+        top: 15,
+        left: 400,
+        fontSize: 18,
+        fontWeight: "semibold",
+        width: 300,
+        textAlign: "start",
+      },
+      {
+        type: "text",
+        children: ["BRADFIELD BADGERFOX"],
+        top: 20,
+        left: 720,
+        fontSize: 10,
+        fontWeight: "normal",
+        width: 230,
+        textAlign: "end",
+      },
+      {
+        type: "text",
+        children: ["3 MYALL AVENUE"],
+        top: 42,
+        left: 400,
+        fontSize: 12,
+        width: 300,
+        textAlign: "start",
+      },
+      {
+        type: "group",
+        top: 80,
+        left: 400,
+        width: 120,
+        height: "auto",
+        children: [
+          {
+            type: "image",
+            top: 0,
+            left: 0,
+            altText: { text: "alt", decorative: false },
+            dataUrl:
+              "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNiIgaGVpZ2h0PSIxNiIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIxLjUiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIgY2xhc3M9Imx1Y2lkZSBsdWNpZGUtYmVkIj48cGF0aCBkPSJNMiA0djE2Ii8+PHBhdGggZD0iTTIgOGgxOGEyIDIgMCAwIDEgMiAydjEwIi8+PHBhdGggZD0iTTIgMTdoMjAiLz48cGF0aCBkPSJNNiA4djkiLz48L3N2Zz4=",
+            height: 15,
+            width: 15,
+          },
+          {
+            type: "text",
+            children: ["3"],
+            top: 0,
+            left: 15,
+            fontSize: 12,
+            textAlign: "start",
+          },
+          {
+            type: "image",
+            top: 0,
+            left: 25,
+            altText: { text: "adadsda", decorative: false },
+            dataUrl:
+              "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMiIgaGVpZ2h0PSIzMiIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIxLjUiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIgY2xhc3M9Imx1Y2lkZSBsdWNpZGUtYmF0aCI+PHBhdGggZD0iTTEwIDQgOCA2Ii8+PHBhdGggZD0iTTE3IDE5djIiLz48cGF0aCBkPSJNMiAxMmgyMCIvPjxwYXRoIGQ9Ik03IDE5djIiLz48cGF0aCBkPSJNOSA1IDcuNjIxIDMuNjIxQTIuMTIxIDIuMTIxIDAgMCAwIDQgNXYxMmEyIDIgMCAwIDAgMiAyaDEyYTIgMiAwIDAgMCAyLTJ2LTUiLz48L3N2Zz4=",
+            height: 15,
+            width: 15,
+          },
+          {
+            type: "text",
+            children: ["3"],
+            top: 0,
+            left: 40,
+            fontSize: 12,
+            textAlign: "start",
+          },
+          {
+            type: "image",
+            top: 0,
+            left: 50,
+            altText: { text: "adadsda", decorative: false },
+            dataUrl:
+              "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNiIgaGVpZ2h0PSIxNiIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIxLjUiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIgY2xhc3M9Imx1Y2lkZSBsdWNpZGUtY2FyLWZyb250Ij48cGF0aCBkPSJtMjEgOC0yIDItMS41LTMuN0EyIDIgMCAwIDAgMTUuNjQ2IDVIOC40YTIgMiAwIDAgMC0xLjkwMyAxLjI1N0w1IDEwIDMgOCIvPjxwYXRoIGQ9Ik03IDE0aC4wMSIvPjxwYXRoIGQ9Ik0xNyAxNGguMDEiLz48cmVjdCB3aWR0aD0iMTgiIGhlaWdodD0iOCIgeD0iMyIgeT0iMTAiIHJ4PSIyIi8+PHBhdGggZD0iTTUgMTh2MiIvPjxwYXRoIGQ9Ik0xOSAxOHYyIi8+PC9zdmc+",
+            height: 15,
+            width: 15,
+          },
+          {
+            type: "text",
+            children: ["3"],
+            top: 0,
+            left: 65,
+            fontSize: 12,
+            textAlign: "start",
+          },
+        ],
+      },
+      {
+        type: "group",
+        top: 130,
+        left: 400,
+        width: 300,
+        height: 70,
+        children: [
+          {
+            top: 10,
+            left: 0,
+            type: "shape",
+            paths: [
+              {
+                d: "M 0 0 H 720 V 720 H 0 L 0 0",
+                fill: {
+                  color: "#ffffff",
+                },
+              },
+            ],
+            width: 300,
+            height: 50,
+            viewBox: {
+              top: 0,
+              left: 0,
+              width: 300,
+              height: 50,
+            },
+          },
+          {
+            type: "image",
+            altText: {
+              text: "Hello",
+              decorative: false,
+            },
+            top: 0,
+            left: 10,
+            height: 70,
+            width: 70,
+            dataUrl:
+              "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABEAAAARCAYAAAA7bUf6AAAAAXNSR0IArs4c6QAAAdRJREFUOE9dlAFrE0EUhGfzM/OLCoJYKoFAqbR0L6UoiKLoT9Pse1Nn3t41NXDcBfa+nZk3e+3z0++eADICJPWAiAAYQAzkCMQYiPgLnv943S4DAL2mAWiGkPvMBHUxCqiFoWeBzohx9p1xBpho6wUs7fn0q5Pck4kCBSiIlQU4JiQKkmMUABMkyNPpZ0cKQgMEkgLD9D+GQbFBzmgkdhOCgvwoyLSzqdkUDau6hOzIUqING5Z2evzeCeyxQQL1PNXY2qpEwOHwmzKzLSytP3zrAPewnQr2FZJ+yUoc6rA9TU0gQQAK8rXDwWq8guRbSFYmnpKB6z3mhLi0x09fOqXEgFLjHf6zlAp2qtKEbMt2uLSH2+eCcPYkVaK3oNSUph1lpfJJnUZNZXJ/7D2h6ejlNZcJmhkpk1JSIVePViVY2t3hvpOoTES+zGZ2x32REvdmAhws0Yil3d7czcZOJbYygclqrcatcBXyeiRSjVVfuLTj+6N74qloZwOmHUx7Lt7YlCj0Oj+EG3t4d+hE7uFd62o+oVpQMAW7KvDBNIRQc93Yj1fXr41dXxR/wtbjsDXYNi4gsnNz9aHDxUvfvDuAnXbTd8ZBj2qypjQrsAPr7Pz7vQBKj7pE+J6clQAAAABJRU5ErkJggg==",
+          },
+          {
+            type: "text",
+            children: ["ALEX PAPA"],
+            top: 25,
+            left: 100,
+            fontSize: 10,
+            fontWeight: "bold",
+            width: 150,
+            textAlign: "start",
+          },
+          {
+            type: "text",
+            children: ["+0414 775 816"],
+            top: 37,
+            left: 100,
+            fontSize: 8,
+            width: 150,
+            textAlign: "start",
+          },
+        ],
+      },
+      {
+        type: "group",
+        top: 212,
+        left: 400,
+        width: 120,
+        height: 25,
+        children: [
+          {
+            top: 0,
+            left: 0,
+            width: 120,
+            height: 25,
+            type: "shape",
+            paths: [
+              {
+                d: "M 12.5 0 H 107.5 A 12.5 12.5 0 0 1 120 12.5 V 12.5 A 12.5 12.5 0 0 1 107.5 25 H 12.5 A 12.5 12.5 0 0 1 0 12.5 V 12.5 A 12.5 12.5 0 0 1 12.5 0 Z",
+                fill: {
+                  color: "#F87F0F",
+                },
+              },
+            ],
+            viewBox: {
+              top: 0,
+              left: 0,
+              width: 120,
+              height: 25,
+            },
+          },
+          {
+            type: "text",
+            children: ["Enquire Now"],
+            top: 6,
+            left: 0,
+            fontSize: 12,
+            width: 120,
+            textAlign: "center",
+            color: "#ffffff",
+          },
+        ],
+      },
+    ],
+  });
+};
